c8358414c6b06cdf9cc6299c4b97ecbed081fa15
Sean McQuay <srmcquay@gmail.com>
2024-06-28 17:32:29 +0000

    Toggled off source mapping.

---
+++ .devcontainer/post-create-command.sh
@@ -0,0 +1,32 @@
+sudo apt update
+
+# Install nvm
+sudo apt install curl -y
+curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
+
+# Configure nvm
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+# Configure Git functionality in bash
+sudo apt install bash-completion -y
+
+curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -o ~/.git-completion.bash
+
+echo "if [ -f ~/.git-completion.bash ]; then" >> ~/.bashrc
+echo "  . ~/.git-completion.bash" >> ~/.bashrc
+echo "fi" >> ~/.bashrc
+
+# Refresh bash after adding nvm and Git changes
+source ~.bashrc
+source /tmp/.bashrc
+
+# Install the latest version of NodeJS 20.x.x
+nvm install 20
+
+# Update NPM
+npm install -g npm@latest
+
+# Install package dependencies
+npm i
